371f608f683a399f45d5c63482ef874a795fdc91
Ruben Garcalia <rubeneitor2@hotmail.com>
2024-11-11 21:49:03 +0100

    cambios para que sea hasta 10000 y ha vuelto a ser funcional el codigo original

--- src/ruben/garcalia/guessthenumber/main/GuessTheNumberRuben.java
+++ src/ruben/garcalia/guessthenumber/main/GuessTheNumberRuben.java
@@ -11,34 +11,34 @@ public class GuessTheNumberRuben {
     Scanner input = new Scanner(System.in);
     List<Integer> numbersList = new ArrayList<>();
 
-//    int randomNumber = (int) (Math.random() * 100);
-//    randomNumber = randomNumber == 0 ? 1 : randomNumber;
-//
-//    int guessedNumber;
-//    int guesses = 0;
-//
-//    System.out.println("Input a number between 1 and 100");
-//
-//    do {
-//      guesses++;
-//      guessedNumber = input.nextInt();
-//
-//      numbersList.add(guessedNumber);
-//
-//      if (guessedNumber == randomNumber) break;
-//
-//      if (guessedNumber < randomNumber) {
-//        System.out.format("The number is \033[32mbigger\033[0m [\033[33m%d\033[0m tries]%n", guesses);
-//      }
-//      else {
-//        System.out.format("The number is \033[32msmaller\033[0m [\033[33m%d\033[0m tries]%n", guesses);
-//      }
-//    } while(guessedNumber != randomNumber);
-//    
-//    String plural = guesses > 1 ? "tries" : "try";
-//    System.out.format("You got it in \033[33m%d\033[0m %s%n", guesses, plural);
-//
-//    input.close();
+    int randomNumber = (int) (Math.random() * 10000);
+    randomNumber = randomNumber == 0 ? 1 : randomNumber;
+
+    int guessedNumber;
+    int guesses = 0;
+
+    System.out.println("Input a number between 1 and 10000");
+
+    do {
+      guesses++;
+      guessedNumber = input.nextInt();
+
+      numbersList.add(guessedNumber);
+
+      if (guessedNumber == randomNumber) break;
+
+      if (guessedNumber < randomNumber) {
+        System.out.format("The number is \033[32mbigger\033[0m [\033[33m%d\033[0m tries]%n", guesses);
+      }
+      else {
+        System.out.format("The number is \033[32msmaller\033[0m [\033[33m%d\033[0m tries]%n", guesses);
+      }
+    } while(guessedNumber != randomNumber);
+    
+    String plural = guesses > 1 ? "tries" : "try";
+    System.out.format("You got it in \033[33m%d\033[0m %s%n", guesses, plural);
+
+    input.close();
 
     GamePreparatorRuben.printErrors(numbersList);
   }
